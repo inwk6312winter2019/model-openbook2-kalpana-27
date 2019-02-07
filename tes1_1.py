b9d0ec0be05f00044716716db3fb7a790195f0f3
puneetverma100 <Puneet.Verma@dal.ca>
2019-02-06 16:26:53 -0800

    updated

--- tes1_1.py
+++ tes1_1.py
@@ -1,5 +1,6 @@
 fin = open("Street_Centrelines.csv","r")
 
+#task1
 def myfile(fin):
   list1 = []
   for line in fin:
@@ -8,3 +9,40 @@ def myfile(fin):
   print(list1)
 
 myfile(fin)
+
+
+
+#Task2
+
+
+def histogram(fin):
+	d={}
+	for line in fin:
+		line=line.split(",")
+		if line[12] not in d:
+			d[line[12]]=1
+		else:
+			d[line[12]]+=1
+	print(d)
+histogram(fin)
+
+
+#task3
+def unique(fin):
+	d={}
+	list1 = []
+	for line in fin:
+		line=line.split(",")
+		if line[11] not in d:
+			d[line[11]]=1
+		else:
+			d[line[11]]+=1
+
+	for key in d.keys():
+		list1.append(key)
+	print(list1)
+
+unique(fin)
+
+ 
+
